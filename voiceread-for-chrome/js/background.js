bc828762cb624c1d68ad29aea10f85dbdeff2264
Amy Liu <liuamy@mit.edu>
2016-02-20 23:28:56 -0500

    Pruned voice and font libraries

--- voiceread-for-chrome/js/background.js
+++ voiceread-for-chrome/js/background.js
@@ -7,7 +7,26 @@ var lastCharIndex = null;
 
 // checks voice is english and has word boundaries
 function isValidVoice(voice) {
-  return (voice.lang.indexOf('en') > -1 && voice.eventTypes.indexOf('word') > -1);
+  return voice.lang.indexOf('en') > -1 && voice.eventTypes.indexOf('word') > -1 && 
+    (voice.voiceName == 'Alex' || 
+    voice.voiceName == 'Daniel' || 
+    voice.voiceName == 'Karen' ||
+    voice.voiceName == 'Moira' ||
+    voice.voiceName ==  'Samantha');
+}
+function getValidFont(font) {
+    if (font.fontId == 'Avenir'  || 
+    font.fontId == 'Calibri' || 
+    font.fontId == 'Cambria' ||
+    font.fontId == 'Courier' ||
+    font.fontId == 'Helvetica'||
+    font.fontId == 'OpenDyslexic' ||
+    font.fontId == 'Times New Roman' ||
+    font.fontId == 'Trebuchet'){
+      return font
+     }
+
+    
 }
 
 //looks like this whole script acts on the background page and sends informatoin to the extension.
@@ -25,38 +44,41 @@ function isValidVoice(voice) {
 chrome.runtime.onConnect.addListener(function(port) {
   console.assert(port.name == "voiceread");
   port.onMessage.addListener(function(msg) { //the msg is sent by the chrome extension--contains info about the user's interaction(buttons pressed)
-  	if (msg.type == "request") {
-      chrome.tts.getVoices( function(voice_list) { // chrome.tts is textospeech. getVoices gets all voices
-        port.postMessage({voices: voice_list.filter(isValidVoice)});  //how do we know its a valid voice?    
+    if (msg.type == "request") {
+      chrome.tts.getVoices( function(voice_list) { 
+        port.postMessage({voices: voice_list.filter(isValidVoice)});
+ //how do we know its a valid voice?    
       });
       chrome.fontSettings.getFontList( function(font_list) {
-        port.postMessage({fonts: font_list});      
+        port.postMessage({fonts: font_list.filter(getValidFont)});      
       }); 
     } else if (msg.type == "speak"){
-    	chrome.tts.speak(msg.selected_text,
-    		{
+      chrome.tts.speak(msg.selected_text,
+        {
           voiceName: msg.voice_name, // 'native', 
           rate: msg.speech_rate,
           requiredEventTypes: ['word'],
-    			onEvent: function(event){
+          onEvent: function(event){
             //unsure what suppressBoundary does
             //also need to think more deeply about what's going on with the char indices.
-    				if (!msg.suppressBoundary && event.charIndex != lastCharIndex && event.type == 'word'){
-    					port.postMessage({evt: 'boundary'});
+            if (!msg.suppressBoundary && event.charIndex != lastCharIndex && event.type == 'word'){
+              port.postMessage({evt: 'boundary'});
               lastCharIndex = event.charIndex; //help why
             }
             if (!msg.suppressBoundary && event.type == 'end'){
               port.postMessage({evt: 'finished'});
             }
-    			}// okay so there are two states: 1) boundary (keep going) 2) finished (stop)
-    		}
-    	);
-  	} else if (msg.type == "resume"){
-  		chrome.tts.resume();
-  	} else if (msg.type == "pause"){
-  		chrome.tts.pause();
-  	} else if (msg.type == "stop"){
-  		chrome.tts.stop();
-  	}
+          }// okay so there are two states: 1) boundary (keep going) 2) finished (stop)
+        }
+      );
+    } else if (msg.type == "resume"){
+      chrome.tts.resume();
+    } else if (msg.type == "pause"){
+      chrome.tts.pause();
+    } else if (msg.type == "stop"){
+      chrome.tts.stop();
+    }
    });
 });
+//Alex, Daniel, FIona, Karen, Moira, Samantha
+//Arial, Avenir, Calibri, cambria, courier, helvetica, open dyslexic, times new romam, trebuchet
